fa9b481d0efdacfd58834a67adb8ada75503fb5e
chan-0305 <rr8080482@gmail.com>
2025-07-07 21:52:48 +0530

    Allow Render domain and set host/port for Vite preview

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,9 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  preview: {
+    allowedHosts: ['todo-app-ha5t.onrender.com'],
+    host: '0.0.0.0',
+    port: 10000
+  }
 });
